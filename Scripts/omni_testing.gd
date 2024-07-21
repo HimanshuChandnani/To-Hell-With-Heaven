475b17f804dfa168ff64686f0e440988b30fed10
OmniBoy-zz <37975839+OmniBoy-zz@users.noreply.github.com>
2024-07-21 17:50:44 +0530

    camera fix

--- Scripts/omni_testing.gd
+++ Scripts/omni_testing.gd
@@ -79,13 +79,13 @@ func _physics_process(delta):
 		8:
 			heaven_s_golden_gate.skew += 0.2
 	if start == 9:
-		if camera.global_position.x < $Rykting/Player.global_position.x:
+		if camera.global_position.x < $Rykting/Player.global_position.x+2:
 			camera.global_position.x += 2
-		if camera.global_position.x > $Rykting/Player.global_position.x:
+		if camera.global_position.x > $Rykting/Player.global_position.x-2:
 			camera.global_position.x -= 2
-		if camera.global_position.y < $Rykting/Player.global_position.y:
+		if camera.global_position.y < $Rykting/Player.global_position.y+2:
 			camera.global_position.y += 2
-		if camera.global_position.y > $Rykting/Player.global_position.y:
+		if camera.global_position.y > $Rykting/Player.global_position.y-2:
 			camera.global_position.y -= 2
 
 func _on_first_timer_timeout():
